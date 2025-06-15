e2c6dcdfd4ae1f86896fccd7762d980d2a7d8b14
yjkh17 <yj.kh17@gmail.com>
2025-06-15 03:08:55 +0300

    Initial Commit

---
+++ Smith/SmithApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmithApp.swift
+//  Smith
+//
+//  Created by Yousef Jawdat on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
